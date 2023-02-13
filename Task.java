471b444b7255ae90b12e12691df9bc97ac479301
SArtTech <s.artyom1990@gmail.com>
2023-02-14 00:54:37 +0300

    sortmerge

--- Task.java
+++ Task.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task {
@@ -22,8 +20,51 @@ public class Task {
             }   
         }
         sc.close();
-        System.out.println(num_arr);
+        System.out.println(num_arr);        
+    }
 
-        
+    public static void sort(int[] num_arr, int length) {
+        if (length < 2) {
+            return;
+        }
+        int q = length / 2;
+        int[] b = new int[q];
+        int[] c = new int[length - q];
+
+        for (int i = 0; i < q; i++) {
+            b[i] = num_arr[i];
+        }
+
+        for (int i = q; i < length; i++) {
+            c[i - q] = num_arr[i];
+
+        }
+
+        sort(b, q);
+        sort(c, length - q);
+
+        merge(num_arr, b, c, q, length - q);
+
+
+    }
+
+    public static void merge(int[] num_arr, int[] b, int[] c, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (b[i] <= c[j]) {
+                num_arr[k++] = b[i++];
+            }
+            else {
+                num_arr[k++] = c[j++];
+            }
+        }
+        while (i < left) {
+            num_arr[k++] = b[i++];
+        }
+        while (j < right) {
+            num_arr[k++] = c[j++];
+        }
+
     }
 }
