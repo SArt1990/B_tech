6132eb64e3d0070727fda7a13798c521fa9f6e7e
SArtTech <s.artyom1990@gmail.com>
2023-02-14 01:38:32 +0300

    test4

--- Task.java
+++ Task.java
@@ -6,11 +6,11 @@ public class Task {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter integer array elements (enter 'e' for end):");
-        ArrayList<Integer> num_arr = new ArrayList<>();
+        ArrayList<Integer> listnum_arr = new ArrayList<>();
         
         while (true) {
             if (sc.hasNextInt()) {
-                num_arr.add(sc.nextInt());
+                listnum_arr.add(sc.nextInt());
             } else {
                 if (!sc.nextLine().equals("e")) {
                     System.out.println("Not right element!");                                                               
@@ -20,16 +20,26 @@ public class Task {
             }   
         }
         sc.close();
-        System.out.println(num_arr);        
+        System.out.println("List:" + listnum_arr);  
+        Integer[] num_arr = new Integer[listnum_arr.size()];
+        listnum_arr.toArray(num_arr);
+
+        /* for (int k = 0; k < listnum_arr.size(); k++) {
+            num_arr[k] = listnum_arr.get(k);
+            System.out.println(listnum_arr.get(k));
+        } */
+        
+        System.out.println("Custom:" + num_arr);
+        sort(num_arr, num_arr.length);
     }
 
-    public static void sort(int[] num_arr, int length) {
+    public static void sort(Integer[] num_arr, int length) {
         if (length < 2) {
             return;
         }
         int q = length / 2;
-        int[] b = new int[q];
-        int[] c = new int[length - q];
+        Integer[] b = new Integer[q];
+        Integer[] c = new Integer[length - q];
 
         for (int i = 0; i < q; i++) {
             b[i] = num_arr[i];
@@ -44,11 +54,13 @@ public class Task {
         sort(c, length - q);
 
         merge(num_arr, b, c, q, length - q);
+        
+        System.out.println(num_arr);
 
 
     }
 
-    public static void merge(int[] num_arr, int[] b, int[] c, int left, int right) {
+    public static void merge(Integer[] num_arr, Integer[] b, Integer[] c, int left, int right) {
 
         int i = 0, j = 0, k = 0;
         while (i < left && j < right) {
